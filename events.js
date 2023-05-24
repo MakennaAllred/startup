c12c7dfe1e5181ea524669b1bfb4116fd3a76e9c
MakennaAllred <makenna.foulkrod@gmail.com>
2023-05-24 14:09:37 -0600

    implemented accordion structure for users

--- events.js
+++ events.js
@@ -14,3 +14,20 @@ const events = [
     {name:"Chandler", title:"pick up Cricket from park"}
 ]
 
+let currentData = users;
+
+function table(data){
+    if(!!data && data.length >1) {
+        currentData = data;
+        const headers = parseHeader(data);
+        const tableElement = generateTable(headers, data);
+        const output = document.getElementById("output");
+        removeAllChildNodes(output);
+        output.appendChild(tableElement);}
+        else{
+            outputData("invalid input", data)
+        }
+    }
+
+
+
