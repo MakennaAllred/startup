972b8e8827ca9d86d3f7a5a4dfaa6c1a314ff39c
MakennaAllred <makenna.foulkrod@gmail.com>
2023-05-24 10:44:49 -0600

    created array for users and for events

---
+++ events.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let users = [
+   {name: "Genny"}, 
+   {name:"Chandler"},
+   {name: "Holly"}
+]
+
+const events = [
+    {name:"Genny", title: "dinner"},
+    {name:"Genny", title:"work meeting"},
+    {name:"Holly", title:"lunch with Mak"},
+    {name:"Chandler", title:"visit math TA"}
+    {name:"Chandler", title:"pick up Cricket from park"}
+]
+
