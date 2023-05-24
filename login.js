a297c91b39c40ed14c0d505526cd4471d4a53894
MakennaAllred <makenna.foulkrod@gmail.com>
2023-05-24 10:25:58 -0600

    dropped calendar from showing, fixed buttons

--- login.js
+++ login.js
@@ -2,10 +2,4 @@ function login() {
   const username = document.querySelector('#name');
   localStorage.setItem('userName', username.value);
   window.location.href = 'notifications.html';
-  username.addEventListener("keydown", function(event){
-    if(event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("Login").click()
-    }
-  })
 }
