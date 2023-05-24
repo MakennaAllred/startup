276d0436b5b4a9176648933dd92adb47370e1f7a
MakennaAllred <makenna.foulkrod@gmail.com>
2023-05-24 09:08:46 -0600

    tried to implement login with enter key

--- login.js
+++ login.js
@@ -2,4 +2,10 @@ function login() {
   const username = document.querySelector('#name');
   localStorage.setItem('userName', username.value);
   window.location.href = 'notifications.html';
+  username.addEventListener("keyup", function(event){
+    if(event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("Login").click()
+    }
+  })
 }
