30666e1c473fcd64671db0a2be340a23466c551e
MakennaAllred <makenna.foulkrod@gmail.com>
2023-05-24 22:51:37 -0600

    finally got columns aligned and accordion works

--- events.js
+++ events.js
@@ -1,80 +1,78 @@
-"use strict";
 
 addEventListener("load", (event) => {
-    function buildAccordionList(users) {
+    function buildAccordionList(users, events) {
       let root = document.getElementById('root');
       let accordion = document.createElement('div');
       accordion.classList.add('accordion');
-      accordion.id = 'accordionPanelStayOpen';
+      accordion.id = 'accordionPanelsStayOpenExample';
   
       users.forEach((user, index) => {
-        // Create the accordion section
         let accordionItem = document.createElement('div');
         accordionItem.classList.add('accordion-item');
   
-        // Create the section header
         let header = document.createElement('h2');
         header.classList.add('accordion-header');
   
-        // Create the section content
         let button = document.createElement('button');
-        button.classList.add('accordion-button');
+        button.classList.add('accordion-button', 'collapsed');
         button.setAttribute('type', 'button');
         button.setAttribute('data-bs-toggle', 'collapse');
         button.setAttribute('data-bs-target', `#accordion-${index}`);
         button.setAttribute('aria-expanded', 'false');
         button.setAttribute('aria-controls', `accordion-${index}`);
         button.innerText = user.name;
-        
+  
         header.appendChild(button);
         accordionItem.appendChild(header);
-
+  
         let collapse = document.createElement('div');
-        collapse.id = 'accordion-${index}';
+        collapse.id = `accordion-${index}`;
         collapse.classList.add('accordion-collapse', 'collapse');
-
+  
         let body = document.createElement('div');
         body.classList.add('accordion-body');
-        body.innerText = 'Events for ${user.name}';
-
-        let eventList = document.createElement('ul');
-      eventList.classList.add('list-group');
-
-      events.forEach((event) => {
-        if (event.name === user.name) {
-          let eventItem = document.createElement('li');
-          eventItem.classList.add('list-group-item');
-          eventItem.innerText = event.title;
-          eventList.appendChild(eventItem);
+  
+        let eventsList = document.createElement('div');
+        eventsList.classList.add('accordion-body');
+  
+        events.forEach((event) => {
+          if (event.name === user.name) {
+            let eventItem = document.createElement('p');
+            eventItem.innerText = event.title;
+            eventsList.appendChild(eventItem);
+          }
+        });
+  
+        if (eventsList.childNodes.length === 0) {
+          let noEventsItem = document.createElement('p');
+          noEventsItem.innerText = 'No events found';
+          eventsList.appendChild(noEventsItem);
         }
-      });
-
-      if (eventList.childNodes.length === 0) {
-        let noEventsItem = document.createElement('li');
-        noEventsItem.classList.add('list-group-item', 'text-muted');
-        noEventsItem.innerText = 'No events found';
-        eventList.appendChild(noEventsItem);
-      }
-
+  
+        body.appendChild(eventsList);
         collapse.appendChild(body);
         accordionItem.appendChild(collapse);
-
-        accordion.appendChild(accordionItem); });
-        
-        root.appendChild(accordion);
-      }
-
-      const users =  [
-        {name:"Genny"},{ name:"Chandler"},{name:"Holly"}
-      ]
-      const events = [
-        { name: "Genny", title: "dinner" },
-        { name: "Genny", title: "work meeting" },
-        { name: "Holly", title: "lunch with Mak" },
-        { name: "Chandler", title: "visit math TA" },
-        { name: "Chandler", title: "pick up Cricket from park" }
-      ];
-    
-      buildAccordionList(users, events);
-    });
+  
+        accordion.appendChild(accordionItem);
+      });
+  
+      root.appendChild(accordion);
+    }
+  
+    const users = [
+      { name: "Genny" },
+      { name: "Chandler" },
+      { name: "Holly" }
+    ];
+  
+    const events = [
+      { name: "Genny", title: "dinner" },
+      { name: "Genny", title: "work meeting" },
+      { name: "Holly", title: "lunch with Mak" },
+      { name: "Chandler", title: "visit math TA" },
+      { name: "Chandler", title: "pick up Cricket from park" }
+    ];
+  
+    buildAccordionList(users, events);
+  });
   
