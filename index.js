c4a078a8b3b80ebe4274fea72222c18e2402a684
MakennaAllred <makenna.foulkrod@gmail.com>
2023-06-02 17:17:03 -0600

    created dbconfig file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.argv.length > 2 ? process.arv[2] : 4000;
+const port = 4000;
 
 //uses json
 app.use(express.json());
