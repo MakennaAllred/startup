c1c161253dbace51d0e939282df552471254e320
MakennaAllred <makenna.foulkrod@gmail.com>
2023-06-01 20:03:48 -0600

    added port and some of the event functions

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ const app = express();
 
 const port = process.argv.length > 2 ? process.arv[2] : 4000;
 
+//uses json
 app.use(express.json());
 
+//for static files
 app.use(express.static('public'));
 
-
+//router for service endpoints
 var apirouter = express.Router();
 app.use(`/api`, apirouter);
 
+//tells what port to use
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
+
 myevents = []
 
-apirouter.get('')
+//get my events
+apirouter.get('/events', (req, res) => {
+    res.send(myevents)
+})
+
+//adding an event
+apirouter.post('/events/add', (req, res) =>)
