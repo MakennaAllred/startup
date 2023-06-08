4a5626603d8fc28c53645bc074da20d68ed89251
MakennaAllred <makenna.foulkrod@gmail.com>
2023-06-08 09:36:03 -0600

    updated database.js to include authentication ftns

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const {MongoClient} = require('mongodb');
 const config = require('./dbConfig.json');
+const uuid = require('uuid');
+const bcrpyt = require('bcrypt');
 
 const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('cs260');
 const eventcollection = db.collection('myevents');
+const usercollection = db.collection('user');
 
 (async function testConnection() {
     await client.connect();
@@ -14,6 +17,25 @@ const eventcollection = db.collection('myevents');
     console.log(`Can't connect to database with ${url} because ${ex.message}`);
     process.exit(1);
 });
+
+function getUser(email){
+    return usercollection.findOne({email:email});
+}
+
+function getUserbyToken(token){
+    return usercollection.findOne({toke: token});
+}
+
+async function createUser(email, password){
+    const hashpass = await bcrpyt.hash(password, 7);
+    const user = {
+        email: email,
+        password: hashpass,
+        token: uuid.v4(),
+    };
+    await usercollection.insertOne(user);
+    return user;
+}
 
 async function addEvent(event) {
     const result = await eventcollection.insertOne(event);
@@ -28,4 +50,4 @@ async function getLatestEvent () {
     const latest = await eventcollection.findOne({}, {sort:{eventtime:-1}});
     return latest;
 }
-module.exports = {addEvent, getEvents, getLatestEvent};
+module.exports = {addEvent, getEvents, getLatestEvent, createUser, getUser, getUserbyToken};
