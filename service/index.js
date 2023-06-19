52dc8da5651ba0f5de7ad33740f79183842bd2f3
MakennaAllred <makenna.foulkrod@gmail.com>
2023-06-19 15:22:56 -0600

    started react implementation by moving files

---
+++ service/index.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const app = express();
+const db = require('./service/database.js');
+const cookieParser = require('cookie-parser');
+const bcrpyt = require('bcrypt');
+const authcookie = 'token';
+const {peerProxy, send} =require('./peerproxy.js');
+
+const port = 4000;
+
+//uses cookies for auth tokens
+app.use(cookieParser());
+
+//uses json
+app.use(express.json());
+
+//for static files
+app.use(express.static('public'));
+
+app.set('trust proxy', true);
+
+//router for service endpoints
+var apirouter = express.Router();
+app.use(`/api`, apirouter);
+
+// get an array of logged-in users
+apirouter.get('/api/loggedin', (req, res) =>{
+    User.find({isLoggedin: true})
+    .then ((users) =>{
+        res.json(users);
+        
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    })
+})
+
+
+//get my events
+apirouter.get('/events/:user', async (req, res) => {
+    let user = req.params.user;
+    // console.log(user);
+    const events = await db.getEvents(user);
+    res.send(events)
+})
+
+//adding an event
+apirouter.post('/events/add', async (req, res) => {
+    console.log("add was called")
+    const {eventuser, eventname, eventtime} = req.body;
+    const event = {eventuser: eventuser, eventname: eventname, eventtime:eventtime};
+    const msg = `${eventuser} scheduled ${eventname} at ${eventtime}`;
+    await db.addEvent(event);
+    send(msg);
+    console.log(msg);
+    res.json({ message: 'Event added successfully!'})
+})
+
+//updating the latest event list
+apirouter.get('/events/latest', async (req,res) =>{
+    const latest = await db.getLatestEvent();
+    res.json(latest);
+})
+
+apirouter.post('/auth/create', async (req,res) => {
+    // console.log(req.body);
+    if (await db.getUser(req.body.email)) {
+        res.status(409).send({msg: 'Already a user'});
+    }
+    else{
+        const user = await db.createUser(req.body.email, req.body.password);
+        setAuthCookie(res, user.token);
+        res.send({
+            id: user._id,
+        });
+    }
+});
+
+//gets token for a specific user
+apirouter.post('/auth/login', async (req,res) => {
+    // console.log('post');
+    const user = await db.getUser(req.body.email);
+    if(user) {
+        if (await bcrpyt.compare(req.body.password, user.password)) {
+            setAuthCookie(res, user.token);
+            res.send({id: user._id});
+            return;
+        }
+    }
+    res.status(401).send({msg: "Unauthorized"});
+});
+
+//delete authentication token if stored in a cookie
+apirouter.delete('/auth/logout/:email', async (req,res) => {
+    const del = await db.logoutuser(req.params.email);
+    res.clearCookie(authcookie);
+    res.status(204).end();
+});
+
+//gets user info
+apirouter.get('/user/:email', async (req, res) => {
+    const user = await db.getUser(req.params.email);
+    if (user) {
+        const token = req?.cookies.token;
+        res.send({
+            email:user.email, authenticated: token === user.token
+        }); return; 
+    }
+    res.status(404).send({msg: "Not found"});
+});
+
+apirouter.get('/cookie', async (req, res) =>{
+    const a = await db.getUserbyToken(req?.cookies.token);
+    console.log(a);
+    res.send(a);
+});
+
+var secureapirouter = express.Router();
+apirouter.use(secureapirouter);
+
+secureapirouter.use(async (req, res, next) => {
+    authtoken = req.cookies[authcookie];
+    const user = await db.getUserbyToken(authcookie);
+    if (user) {
+        next()
+    }
+    else{
+        res.status(401).send({msg: "Unauthorized"});
+    }
+});
+
+function setAuthCookie(res, authtoken){
+    res.cookie(authcookie, authtoken,{
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+//tells what port to use
+const httpService = app.listen(port, () => {
+    // console.log(`listening on port ${port}`);
+});
+
+peerProxy(httpService);
--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-    return <div className ='body bg-dark text-light'>App will display here</div>;
+    return (
+        <div class='app'>
+    <header>
+        <h1>Cal Share</h1>
+        <!--this is for the buttons at the top of the page to navigate through app-->
+        <nav id="eventcontrols">
+            <a class="btn btn-primary" href="index.html" role="button"> Home</a>
+            <a class="btn btn-primary" href="events.html" role="button"> Events</a>
+            <!-- <a class="btn btn-primary" href="calendar.html" role="button"> Calendar</a> -->
+            <a class="btn btn-primary"href="proposal.html" role="button"> Add New Event</a>
+        </nav>
+    </header>
+    <!--using main instead of body because it will be the main content of the webpage here-->
+    <main>
+        components go here
+    </main>
+    <footer>
+        <a class='footer' href="https://github.com/MakennaAllred/startup">Makenna Allred's Github</a>
+    </footer>
+    </div>
+    )
 }
