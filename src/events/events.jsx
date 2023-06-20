ad7455b2631f82af23421488ba61d5a69dfbf617
MakennaAllred <makenna.foulkrod@gmail.com>
2023-06-20 15:04:49 -0600

    got holidays to display

--- src/events/events.jsx
+++ src/events/events.jsx
@@ -4,6 +4,7 @@ export function Events() {
    //display users on events page of logged in users
    const [users, setUsers] = React.useState([]);
    const [events, setEvents] = React.useState([]);
+   const [holidays, setHolidays] = React.useState([]);
    React.useEffect(() => {
        fetch('/api/cookie')
            .then((response) => response.json())
@@ -39,7 +40,6 @@ export function Events() {
            fetch(`/api/events/${user}`)
            .then((response) =>response.json())
            .then((events) => {
-                console.log(events);
                setEvents(events);
                localStorage.setItem('events', JSON.stringify(events));
            })
@@ -51,8 +51,9 @@ export function Events() {
            });
        }, []);
 
+       
+
        const eventslist = [];
-       console.log(events.length);
        if(events.length){
         for (const [i, event] of events.entries()){
             eventslist.push(
@@ -61,17 +62,45 @@ export function Events() {
        }
     
 
+        // holidays
+        React.useEffect(() => {
+            fetch('https://date.nager.at/api/v3/PublicHolidays/2023/US')
+            .then((response) => response.json())
+            .then((holidays) => {
+                console.log(holidays);
+              holidaylist.push(holidays);
+              setHolidays(holidays);
+            })
+            .catch((error) => {
+              console.error('Error:' , error)
+            });
+          }, [])
+
+          const holidaylist = []
+          if(holidays.length){
+            for (const [i, holiday] of holidays.entries()){
+                holidaylist.push(
+                    <li key={i}>{holiday.localName} on {holiday.date}</li>
+                )
+            }
+          }
+
+
+
+       
+
     return (
     <main>
         <div className="container text-center">
             <div className="row">
               <div className="col">
                 <h3>Users</h3>
-                <ul id="loggedusers"> {userslist}</ul>
+                <ul>{userslist}</ul>
               </div>
               <div className="col">
                 <h3>My Events</h3>
                   <ul id ="myevents"> {eventslist} </ul>
+                  <ul>{holidaylist}</ul>
               </div>
               <div className="col">
                 <h3>Latest Events</h3>
