b97c37eac0cd8cf46a369961beb989e39f07eb1c
MakennaAllred <makenna.foulkrod@gmail.com>
2023-06-20 12:56:16 -0600

    formatted correctly

--- src/app.jsx
+++ src/app.jsx
@@ -13,65 +13,6 @@ function NotFound(){
 
 
 export default function App() {
-    //display users on events page of logged in users
-    const [users, setUsers] = React.useState([]);
-    React.useEffect(() => {
-        fetch('/api/cookie')
-            .then((response) => response.json())
-            .then((users) => {
-                setUsers(users);
-                localStorage.setItem('users', JSON.stringify(users));
-            })
-            .catch(() => {
-                const usersText = localStorage.getItem('users');
-                if (usersText){
-                    setUsers(JSON.parse(usersText));
-                }
-            });
-        },[]);
-
-        const userslist = [];
-            if (users.length){
-                for (const [i,users] of users.entries()){
-                    userslist.push (
-                    <li>{users.name[i]}</li>
-                );
-            }
-        } else{
-            userslist.push(
-            <li>No users logged in</li>
-            );
-        }
-
-        // display my events on the events page
-        const[events, setEvents] = React.useState([]);
-
-        React.useEffect(() => {
-            let user = localStorage.getItem('userName');
-            fetch(`/api/events/${user}`)
-            .then((response) =>response.json())
-            .then((events) => {
-                setEvents(events);
-                localStorage.setItem('events', JSON.stringify(events));
-            })
-            .catch(() => {
-                const eventsText = localStorage.getItem('events');
-                if (eventsText) {
-                    setEvents(JSON.parse(eventsText));
-                }
-            });
-        }, []);
-
-        const eventslist = [];
-        if (events.length) {
-            for (const [i,events] of events.entries()){
-                eventslist.push(
-                    <li>{events[i]}</li>
-                )
-            }
-        }
-
-        
     return (
         <BrowserRouter>
     <div className='app'>
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export function Login() {
+    
     return (
     <main>
         <div id ="logincontrols">
